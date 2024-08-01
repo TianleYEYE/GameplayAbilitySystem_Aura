742fdb1673fbda9eaeab30deaabf563603632900
TianleYEYE <3484768630@qq.com>
2024-08-02 01:21:36 +0800

    Player Controller

---
+++ Source/Aura/Private/Player/AuraPlayerController.cpp
@@ -0,0 +1,58 @@
+﻿// Tian Le
+
+
+#include "Player/AuraPlayerController.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+
+
+AAuraPlayerController::AAuraPlayerController()
+{
+	bReplicates = true;
+}
+
+void AAuraPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	check(AuraContext);
+	
+	UEnhancedInputLocalPlayerSubsystem* Subsystem=ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer());
+	check(Subsystem);
+	Subsystem->AddMappingContext(AuraContext,0);//存在多个操作映射，设置AuraContext为优先触发
+	
+	bShowMouseCursor= true;//显示鼠标
+	DefaultMouseCursor =EMouseCursor::Default;//默认鼠标光标样式
+
+	FInputModeGameAndUI InputModeData;
+	InputModeData.SetLockMouseToViewportBehavior(EMouseLockMode::DoNotLock);//
+	InputModeData.SetHideCursorDuringCapture(false);
+	SetInputMode(InputModeData);
+}
+
+void AAuraPlayerController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+
+	UEnhancedInputComponent* EnhancedInputComponent =CastChecked<UEnhancedInputComponent>(InputComponent);
+
+	EnhancedInputComponent->BindAction(MoveAction,ETriggerEvent::Triggered,this,&AAuraPlayerController::Move);
+}
+
+void AAuraPlayerController::Move(const FInputActionValue& InputActionValue)
+{
+	const FVector2D InputAxisVector =InputActionValue.Get<FVector2D>();
+	const FRotator Rotation = GetControlRotation();
+	const FRotator YawRotator(0.f,Rotation.Yaw,0.f);
+
+	const FVector ForwardDirection = FRotationMatrix(YawRotator).GetUnitAxis(EAxis::X);
+	const FVector RightDirection = FRotationMatrix(YawRotator).GetUnitAxis(EAxis::Y);
+
+	if (APawn *ControllerPawn = GetPawn<APawn>())
+	{
+		ControllerPawn->AddMovementInput(ForwardDirection,InputAxisVector.Y);//修改Y方向的坐标
+		ControllerPawn->AddMovementInput(RightDirection,InputAxisVector.X);//修改X方向的坐标
+	}
+	
+}
+
